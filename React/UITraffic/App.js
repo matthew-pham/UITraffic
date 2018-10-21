657e5a39c03fccdc08805933c6ce4820d7f8e81a
Matthew Pham <matthewpham55@gmail.com>
2018-10-21 14:27:35 -0500

    Example POST request in JS

--- React/UITraffic/App.js
+++ React/UITraffic/App.js
@@ -1,21 +1,52 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Constants, Location, Permissions } from 'expo';
 
 export default class App extends React.Component {
-	state = {
-		output: "", 
-		flag: false
-	};
+  state = {
+	output: "", 
+	lag: false
+    };
+   
+   _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
 
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+	
+	
   render() {
+	  this._getLocationAsync()
+	   let text = '';
+	   let longitude = ''
+	   let latitude = ''
+	   
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      //text = JSON.stringify(this.state.location);
+	  text = JSON.stringify(this.state.location.coords.longitude);
+	  longitude = "Longitude: " + JSON.stringify(this.state.location.coords.longitude);
+	  latitude = "Latitude: " + JSON.stringify(this.state.location.coords.latitude);
+    }
+	/*
 		if(!this.state.flag){
 		this.state.flag = true
 		this.getLocations()
 		}
+		*/
 		return(
 			<View style={styles.container}>
 			<Text>UI Traffic!</Text>
-			<Text>{this.state.output}</Text>
+			
+			 <Text>{longitude}</Text>
+			  <Text>{latitude}</Text>
 			</View>
 		);
 	}
