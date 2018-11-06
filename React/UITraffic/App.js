543dde302d2133e53423213ec1c53b44da9bf1d3
kevinPal <kevinpalani@gmail.com>
2018-11-05 23:14:28 -0600

    Fixed spacing, merged map

--- React/UITraffic/App.js
+++ React/UITraffic/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native';
-import MapView from 'react-native-maps';
-import {Constants, Location, Permissions } from 'expo';
-import { createBottomTabNavigator } from 'react-navigation'
-import { setCustomText } from 'react-native-global-props'
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import MapView from "react-native-maps";
+import { Constants, Location, Permissions } from "expo";
+import { createBottomTabNavigator } from "react-navigation";
+import { setCustomText } from "react-native-global-props";
 
 export class Home extends React.Component {
   state = {
-	output: "", 
-	flag: false,
-	markers: [],
-	globalLongitude: "",
-	globalLatitude: "",
-	testLon: "",
-	testLat:""
-    };
-   
-   _getLocationAsync = async () => {
+    output: "",
+    flag: false,
+    markers: [],
+    globalLongitude: "",
+    globalLatitude: "",
+    testLon: "",
+    testLat: ""
+  };
+
+  _getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== "granted") {
       this.setState({
@@ -29,23 +29,33 @@ export class Home extends React.Component {
   };
 
   render() {
-			if(!this.state.flag){
-			this.state.flag = true;
-			this.handleLocation();
-			}
-		return(
-			<View style={styles.content}>
-			<Text style ={styles.test}>{"\n"}UI Traffic!{"\n"}</Text>
-			 <Text style = {styles.test2}>{this.state.globalLongitude}</Text>
-			  <Text style = {styles.test2}>{this.state.globalLatitude}{"\n"}</Text>
-			  <Image
-          style={{width: 226, height: 327}}
-          source={{uri: 'https://sustainability.illinois.edu/wp-content/themes/Divi-child/images/ui_logo.png'}}
+    if (!this.state.flag) {
+      this.state.flag = true;
+      this.handleLocation();
+    }
+    return (
+      <View style={styles.content}>
+        <Text style={styles.test}>
+          {"\n"}
+          UI Traffic!
+          {"\n"}
+        </Text>
+        <Text style={styles.test2}>{this.state.globalLongitude}</Text>
+        <Text style={styles.test2}>
+          {this.state.globalLatitude}
+          {"\n"}
+        </Text>
+        <Image
+          style={{ width: 226, height: 327 }}
+          source={{
+            uri:
+              "https://sustainability.illinois.edu/wp-content/themes/Divi-child/images/ui_logo.png"
+          }}
         />
-			</View>
-		);
-	}
-  
+      </View>
+    );
+  }
+
   /*
 	Gets data from database
   */
@@ -86,20 +96,19 @@ export class Home extends React.Component {
         "}"
     );
 
-	   if(this.state.errorMessage) {
-		//Permissions error
-	   }else if (this.state.location) {
-		 this.state.globalLongitude = "Longitude: " + JSON.stringify(this.state.location.coords.longitude);
-		 this.state.globalLatitude = "Latitude: " + JSON.stringify(this.state.location.coords.latitude);
-		 this.state.testLon = JSON.stringify(this.state.location.coords.longitude);
-		 this.state.testLat = JSON.stringify(this.state.location.coords.latitude);
-		 //aggregatedLocations = getLocations();
+    if (this.state.errorMessage) {
+      //Permissions error
+    } else if (this.state.location) {
+      this.state.globalLongitude =
+        "Longitude: " + JSON.stringify(this.state.location.coords.longitude);
+      this.state.globalLatitude =
+        "Latitude: " + JSON.stringify(this.state.location.coords.latitude);
+      this.state.testLon = JSON.stringify(this.state.location.coords.longitude);
+      this.state.testLat = JSON.stringify(this.state.location.coords.latitude);
+      //aggregatedLocations = getLocations();
     }
-		setTimeout(this.handleLocation, 5000);
-	}
-	
-	
-	
+    setTimeout(this.handleLocation, 5000);
+  };
 
   handleLocation = () => {
     //var aggregatedLocations;
@@ -121,52 +130,53 @@ export class Home extends React.Component {
   };
 }
 
-	/*
+/*
 		Map screen
 	*/
-	export class Map extends Component {
-		render(){
-		return(
-			<MapView style = {styles.map}
-            initialRegion = {{
-                latitude: 13.139238380834923,
-                longitude: 80.25188422300266,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-                }}/>
-		);	
-		}
-	}
-
-
-  export default createBottomTabNavigator({
-	Home: Home,
-	Map: Map
-  });
-  const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     backgroundColor: '#fff',
-     alignItems: 'center',
-     justifyContent: 'center',
-    },
-	content:{
-	 flex: 1,
-	 backgroundColor: 'rgba(19, 41, 75, 1)',
-	 alignItems: 'center',
-	},
-	test:{
-	 fontSize: 48,
-	 fontFamily: "Chalkduster",
-	 color: 'rgba(232, 74, 39, 1)'
-	},
-	test2:{
-	  fontSize: 24,
-	 fontFamily: "Cochin",
-	 color: 'rgba(232, 74, 39, 1)'
-	},
-	   map: {
-        height: 100,
-        flex: 1
+export class Map extends Component {
+  render() {
+    return (
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 40.114883,
+          longitude: -88.228081,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      />
+    );
   }
-  });
+}
+
+export default createBottomTabNavigator({
+  Home: Home,
+  Map: Map
+});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "rgba(19, 41, 75, 1)",
+    alignItems: "center"
+  },
+  test: {
+    fontSize: 48,
+    fontFamily: "Chalkduster",
+    color: "rgba(232, 74, 39, 1)"
+  },
+  test2: {
+    fontSize: 24,
+    fontFamily: "Cochin",
+    color: "rgba(232, 74, 39, 1)"
+  },
+  map: {
+    height: 100,
+    flex: 1
+  }
+});
