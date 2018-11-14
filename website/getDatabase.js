c44813c88e0c9f6ed0c0299795d9243358ef7d05
jachen20 <jc27@illinois.edu>
2018-11-14 01:34:28 -0600

    attempt at using points from database for map

--- website/getDatabase.js
+++ website/getDatabase.js
@@ -1,5 +1,6 @@
+var xhttp = new XMLHttpRequest();
+
 function loadDoc() {
-  var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("post").innerHTML = this.responseText;
--- website/map.js
+++ website/map.js
@@ -14,12 +14,10 @@
       // Heatmap data: 500 Points
       function getPoints() {
         return [
-          new google.maps.LatLng(37.782551, -122.445368),
-          new google.maps.LatLng(37.782745, -122.444586),
-          new google.maps.LatLng(37.782842, -122.443688),
-          new google.maps.LatLng(37.782919, -122.442815),
-          new google.maps.LatLng(37.782992, -122.442112),
-          new google.maps.LatLng(37.783100, -122.441461),
-          new google.maps.LatLng(37.783206, -122.440829)
+            for (var key in xhttp) {
+                if (xhttp.hasOwnProperty(key)) {
+                    new google.maps.LatLng(xhttp[key].latitude, xhttp[key].longitude)
+                }
+            }
         ];
       }
