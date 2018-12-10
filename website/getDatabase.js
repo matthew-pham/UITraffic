2cf157b508c9d172df20a29d39b202d0b6f47c69
jachen20 <jc27@illinois.edu>
2018-12-10 01:58:55 -0600

    set up variables in getDatabase.js and gave example pseudocode

--- website/getDatabase.js
+++ website/getDatabase.js
@@ -7,6 +7,11 @@ function loadDoc() {
       document.getElementById("post").innerHTML = this.responseText;
       json = JSON.parse(this.responseText);
      json.forEach(function(object) {
+         //sliced the necessary parts of object.time (string) and put them into variables
+         var hour = object.time.slice(-8, -6);
+         var minute = object.time.slice(-5, -3);
+         
+         //what needs to be added below here is an if statement saying that as long as hour is between hours1 and hours2 (hours1 and hours2 are found in rangeSlider.js and they represent the hours the user has chosen on the slider) and the same for minute, then you can do the below
         coordinates.push(new google.maps.LatLng(object.latitude,object.longitude));
       });
       
