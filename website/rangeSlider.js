6f32bfca6c432f2664e85f12137821e92d1716e4
kevinpal <kevinpalani@gmail.com>
2018-12-12 01:53:05 +0000

    slider fixes and debug info

--- website/rangeSlider.js
+++ website/rangeSlider.js
@@ -1,7 +1,7 @@
-var hours1;
-var hours2;
-var minutes1;
-var minutes2;
+var hours1 = 9;
+var hours2 = 0;
+var minutes1 = 17;
+var minutes2 = 0;
 $("#slider-range").slider({
     range: true,
     min: 0,
@@ -15,25 +15,6 @@ $("#slider-range").slider({
         if (hours1.length == 1) hours1 = '0' + hours1;
         if (minutes1.length == 1) minutes1 = '0' + minutes1;
         if (minutes1 == 0) minutes1 = '00';
-	/*
-        if (hours1 >= 12) {
-            if (hours1 == 12) {
-                hours1 = hours1;
-                minutes1 = minutes1 + " PM";
-            } else {
-                hours1 = hours1 - 12;
-                minutes1 = minutes1 + " PM";
-            }
-        } else {
-            hours1 = hours1;
-            minutes1 = minutes1 + " AM";
-        }
-        if (hours1 == 0) {
-            hours1 = 12;
-            minutes1 = minutes1;
-        }
-	*/
-
 
 
         $('.slider-time').html(hours1 + ':' + minutes1);
@@ -44,23 +25,7 @@ $("#slider-range").slider({
         if (hours2.length == 1) hours2 = '0' + hours2;
         if (minutes2.length == 1) minutes2 = '0' + minutes2;
         if (minutes2 == 0) minutes2 = '00';
-	/*
-        if (hours2 >= 12) {
-            if (hours2 == 12) {
-                hours2 = hours2;
-                minutes2 = minutes2 + " PM";
-            } else if (hours2 == 24) {
-                hours2 = 11;
-                minutes2 = "59 PM";
-            } else {
-                hours2 = hours2 - 12;
-                minutes2 = minutes2 + " PM";
-            }
-        } else {
-            hours2 = hours2;
-            minutes2 = minutes2 + " AM";
-        }
-	*/
+
 
         $('.slider-time2').html(hours2 + ':' + minutes2);
     }
